9ade8437359bd6f6a884aef6612973bc19b64981
Gitoler <45626176+Gitoler@users.noreply.github.com>
2021-08-13 17:05:47 +0700

    first server

---
+++ api/WebApplication1/WebApplication1/Controllers/HeroController.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public HeroController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select HeroId as ""HeroId"",
+                        HeroName as ""HeroName"",
+                        HeroPower as ""HeroPower"",
+                        HeroAlterEgo as ""HeroAlterEgo""
+                from Hero
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"
+                select HeroId as ""HeroId"",
+                        HeroName as ""HeroName"",
+                        HeroPower as ""HeroPower"",
+                        HeroAlterEgo as ""HeroAlterEgo""
+                from Hero where HeroId = @HeroId
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@HeroId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Hero hero)
+        {
+            string query = @"
+                insert into Hero (HeroName,HeroPower,HeroAlterEgo) 
+                values               (@HeroName,@HeroPower,@DHeroAlterEgo) 
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+
+                    myCommand.Parameters.AddWithValue("@HeroName", hero.HeroName);
+                    myCommand.Parameters.AddWithValue("@HeroPower", hero.HeroPower);
+                    myCommand.Parameters.AddWithValue("@HeroAlterEgo", hero.HeroAlterEgo);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Hero hero)
+        {
+            string query = @"
+                update Hero
+                set HeroName = @HeroName,
+                HeroPower = @HeroPower,
+                HeroAlterEgo = @HeroAlterEgo,
+                where HeroId=@HeroId 
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@HeroId", hero.HeroId);
+                    myCommand.Parameters.AddWithValue("@HeroName", hero.HeroName);
+                    myCommand.Parameters.AddWithValue("@HeroPower", hero.HeroPower);
+                    myCommand.Parameters.AddWithValue("@HeroAlterEgo", hero.HeroAlterEgo);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                delete from Hero
+                where HeroId=@HeroId 
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@HeroId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Hero
+    {
+        public int HeroId { get; set; }
+        public string HeroName { get; set; }
+        public string HeroPower { get; set; }
+
+        public string HeroAlterEgo { get; set; }
+
+    }
+}
